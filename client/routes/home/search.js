c508f3fe9b8521abff2daac710fd3c9b12592a8e
Matt Ross <matt@smartsrc.com>
2016-04-12 20:54:38 -0700

    code cleanup

--- client/routes/home/search.js
+++ client/routes/home/search.js
@@ -1,78 +1,24 @@
 import React, { Component } from 'react';
 import ReactDOM, { findDOMNode } from 'react-dom';
-
-const methodSuggestions = [
-  'find()',
-  'limit()',
-  'remove()',
-  'sort()',
-  'toArray()',
-  'update()',
-];
-
-const operatorSuggestions = [
-  'geoNear',
-  'group',
-  'in',
-  'match',
-  'set',
-  'sum',
-  'sort',
-]
-
-const generalSuggestions = [
-  'ObjectID()',
-]
-
-const getSuggestions = (collections, search, caret) => {
-  if (search.length < 3) return [];
-
-  const typing = search.substring(0, caret);
-
-  const collectionCheck = checkSuggestions(collections, 'db.', typing, search, 0);
-  if (collectionCheck.length > 0) return collectionCheck;
-
-  const methodCheck = checkSuggestions(methodSuggestions, '.', typing, search, -1);
-  if (methodCheck.length > 0) return methodCheck;
-
-  const operatorCheck = checkSuggestions(operatorSuggestions, '$', typing, search, 0);
-  if(operatorCheck.length > 0) return operatorCheck;
-
-  const generalCheck = checkSuggestions(generalSuggestions, ' ', typing, search, -1);
-  if (generalCheck.length > 0) return generalCheck;
-
-  return [];
-};
-
-const checkSuggestions = (arr, base, typing, fullsearch, caretOffset) => {
-  if (typing.indexOf(base) === -1) return [];
-
-  const trimmedFull = fullsearch.substring(typing.lastIndexOf(base))
-  const search = typing.substring(typing.lastIndexOf(base));
-
-  if (search.length < base.length + 1) return [];
-
-  return arr.filter(v => {
-    const compare = `${base}${v.toLowerCase()}`;
-    if (trimmedFull.substring(0, compare.length).trim() === compare.trim()) return false;
-    if (compare.trim() === search.trim()) return false;
-    if (compare.indexOf(search) === 0) return true;
-    return false;
-  }).map(v => {
-    const compare = `${base}${v.toLowerCase()}`;
-    return {
-      base,
-      prev: v.substring(0, (search.length - base.length)),
-      next: v.substring(search.length - base.length, v.length),
-      caretOffset }
-  })
-};
+import {
+  methodSuggestions,
+  operatorSuggestions,
+  generalSuggestions,
+  getSuggestions,
+  getCaret,
+  setSelectionRange,
+  specialCodes,
+  specialCodesDic,
+  shiftCodes,
+  shiftCodesDic,
+} from 'utils/search-utils';
 
 class Search extends Component {
   constructor (props) {
     super(props);
     this.handleKeyDown = this.handleKeyDown.bind(this);
-    this.state = { activeSuggestion: 0 };
+    this.state = { activeSuggestion: 0, showSuggestion: false };
+    this.suggestions = [];
   }
   componentDidMount() {
     window.addEventListener('keydown', this.handleKeyDown)
@@ -82,79 +28,37 @@ class Search extends Component {
     const { keyCode, shiftKey } = e;
     const { value } = e.target;
     const { activeSuggestion } = this.state;
-    const { suggestions } = this;
-
-    var caret = getCaret(this.refs.search);
-
-    const specialCodes = [
-      186, // ;
-      222, // '
-      221, // ]
-      219, // [
-      190, // .
-    ]
-
-    const specialCodesDic = [
-      ';',
-      "'",
-      ']',
-      '[',
-      '.',
-    ]
-
-    const shiftCodes = [
-      57, // (
-      48, // )
-      186, // :
-      222, // "
-      221, // }
-      219, // {
-    ]
-
-    const shiftCodesDic = [
-      '(',
-      ')',
-      ':',
-      '"',
-      '}',
-      '{',
-    ]
+    const { suggestions, caret } = this;
 
     if (!shiftKey && specialCodes.indexOf(keyCode) !== -1) {
       if (this.props.value[caret] === specialCodesDic[specialCodes.indexOf(keyCode)]) {
         e.preventDefault();
-        setSelectionRange(this.refs.search, caret + 1, caret + 1);
-        return false;
+        return setSelectionRange(this.refs.search, caret + 1, caret + 1);
       }
     }
 
     if (shiftKey && shiftCodes.indexOf(keyCode) !== -1) {
       if (this.props.value[caret] === shiftCodesDic[shiftCodes.indexOf(keyCode)]) {
         e.preventDefault();
-        setSelectionRange(this.refs.search, caret + 1, caret + 1);
-        return false;
+        return setSelectionRange(this.refs.search, caret + 1, caret + 1);
       }
     }
 
     if (suggestions.length > 0 && (keyCode === 40 || keyCode === 38 || keyCode === 13)) {
       e.preventDefault();
-      if (keyCode === 40) {
-        this.setState({ activeSuggestion: activeSuggestion + 1 });
-        return false;
+      if (keyCode === 40 && activeSuggestion + 1 < suggestions.length) {
+        return this.setState({ activeSuggestion: activeSuggestion + 1 });
       }
-      if (keyCode === 38) {
-        this.setState({ activeSuggestion: activeSuggestion - 1 });
-        return false;
+      if (keyCode === 38 && activeSuggestion > 0) {
+        return this.setState({ activeSuggestion: activeSuggestion - 1 });
       }
       if (keyCode === 13) {
-        this.chooseSuggestion(suggestions[activeSuggestion].next, suggestions[activeSuggestion].caretOffset);
-        return false;
+        return this.chooseSuggestion(suggestions[activeSuggestion].next, suggestions[activeSuggestion].caretOffset);
       }
     }
-    return true;
   }
-  render() {
-    const { handleOnChange, value, handleSendQuery, currentDb } = this.props;
+  componentWillReceiveProps(props) {
+    const { value, currentDb } = props;
     const { activeSuggestion } = this.state;
 
     const collections = currentDb ?
@@ -162,32 +66,48 @@ class Search extends Component {
 
     const search = value.toLowerCase();
 
-    var caret = getCaret(this.refs.search);
+    this.caret = getCaret(this.refs.search);
 
-    this.suggestions = getSuggestions(collections, search, caret);
+    this.suggestions = getSuggestions(collections, search, this.caret);
+
+    if (this.suggestions.length < activeSuggestion + 1) this.setState({ activeSuggestion: 0 });
+  }
+  render() {
+    const { handleOnChange, value, handleSendQuery, currentDb } = this.props;
+    const { activeSuggestion, showSuggestion } = this.state;
+
+    const { caret } = this;
+
+    const collections = currentDb ?
+      currentDb.collections.map(v => v.name) : [];
 
     this.chooseSuggestion = (suggestion, caretOffset) => {
       handleOnChange(`${value.substring(0, caret)}${suggestion}${value.substring(caret, value.length)}`);
       setTimeout(() => {
         const position = caret + caretOffset + suggestion.length;
         setSelectionRange(this.refs.search, position, position);
+        this.setState({ showSuggestion: false });
       })
     }
 
     const { suggestions, chooseSuggestion } = this;
 
     const handleSearchChange = (e) => {
+      if (showSuggestion === false) this.setState({ showSuggestion: true });
       handleOnChange(e.target.value);
     }
 
-
     const { offsetHeight, scrollTop } = this.refs.search ? this.refs.search : { offsetHeight: 0, scrollTop: 0 };
 
+    const handleOnFocus = () => this.setState({ showSuggestion: true });
+    const handleOnBlur = () => this.setState({ showSuggestion: false });
 
     return (
       <div style={{ position: 'relative' }}>
         <div className="input-group">
           <textarea
+            onFocus={handleOnFocus}
+            onBlur={handleOnBlur}
             ref="search"
             className="form-control"
             type="text"
@@ -199,18 +119,13 @@ class Search extends Component {
           </textarea>
           <span className="input-group-addon" id="qyinput" onClick={handleSendQuery}>Send</span>
         </div>
-        {suggestions.length > 0 && (
-          <div>
-            {getSuggestionHtml(suggestions, value, activeSuggestion, caret, chooseSuggestion, scrollTop)}
-          </div>
-        )}
+        {suggestions.length > 0 && showSuggestion && getSuggestionHtml(suggestions, value, activeSuggestion, caret, chooseSuggestion, scrollTop)}
       </div>
     )
   }
 }
 
 const formatValue = (value) => value.split('\n').map(v => <span>{v}<br/></span>)
-
 
 const getSuggestionHtml = (suggestions, value, activeSuggestion, caret, chooseSuggestion, scrollTop) => {
 
@@ -260,42 +175,6 @@ const getSuggestionHtml = (suggestions, value, activeSuggestion, caret, chooseSu
       </div>
     </div>
   )
-}
-
-function getCaret(el) {
-  if(!el) return null;
-  if (el.selectionStart) {
-    return el.selectionStart;
-  } else if (document.selection) {
-    el.focus();
-
-    var r = document.selection.createRange();
-    if (r == null) {
-      return 0;
-    }
-
-    var re = el.createTextRange(),
-        rc = re.duplicate();
-    re.moveToBookmark(r.getBookmark());
-    rc.setEndPoint('EndToStart', re);
-
-    return rc.text.length;
-  }
-  return 0;
-}
-
-function setSelectionRange(input, selectionStart, selectionEnd) {
-  if (input.setSelectionRange) {
-    input.focus();
-    input.setSelectionRange(selectionStart, selectionEnd);
-  }
-  else if (input.createTextRange) {
-    var range = input.createTextRange();
-    range.collapse(true);
-    range.moveEnd('character', selectionEnd);
-    range.moveStart('character', selectionStart);
-    range.select();
-  }
 }
 
 export default Search;
