ef3adbd8d367cca39f3dd49db2315873a3773fe0
Matt Ross <matt@smartsrc.com>
2016-06-13 08:34:54 -0700

    formatted json in textarea

--- client/routes/home/search.js
+++ client/routes/home/search.js
@@ -17,48 +17,106 @@ class Search extends Component {
   constructor (props) {
     super(props);
     this.handleKeyDown = this.handleKeyDown.bind(this);
+    this.handleOnScroll = this.handleOnScroll.bind(this);
+    this.handleSetCaret = this.handleSetCaret.bind(this);
     this.state = { activeSuggestion: 0, showSuggestion: false };
     this.suggestions = [];
   }
   componentDidMount() {
     window.addEventListener('keydown', this.handleKeyDown)
   }
+  componentDidUpdate() {
+    const caret = getCaret(this.refs.search);
+    if (caret !== this.state.caret) this.setState({ caret });
+  }
   handleKeyDown(e) {
     // 40 arrow down, 38 arrow up, 13 enter
-    const { keyCode, shiftKey } = e;
+    const { keyCode, shiftKey, ctrlKey } = e;
     const { value } = e.target;
-    const { activeSuggestion } = this.state;
+    const { handleSendQuery } = this.props;
+    const { activeSuggestion, showSuggestion, caret } = this.state;
     const { suggestions } = this;
 
-    const caret = getCaret(this.refs.search)
+    if (keyCode === 37 || keyCode === 39) {
+      this.suggestions = [];
+      this.setState({ showSuggestion: false });
+      setTimeout(this.forceUpdate.bind(this), 50)
+      return;
+    }
 
     if (!shiftKey && specialCodes.indexOf(keyCode) !== -1) {
       if (this.props.value[caret] === specialCodesDic[specialCodes.indexOf(keyCode)]) {
         e.preventDefault();
-        return setSelectionRange(this.refs.search, caret + 1, caret + 1);
+        setSelectionRange(this.refs.search, caret + 1, caret + 1);
+        setTimeout(this.forceUpdate.bind(this), 50)
+        return
+      }
+      if ('[' === specialCodesDic[specialCodes.indexOf(keyCode)]) {
+        e.preventDefault();
+        this.chooseSuggestion('[]', -1)
+        return
+      }
+      if ('\'' === specialCodesDic[specialCodes.indexOf(keyCode)]) {
+        e.preventDefault();
+        this.chooseSuggestion('\'\'', -1)
+        return
       }
     }
 
     if (shiftKey && shiftCodes.indexOf(keyCode) !== -1) {
       if (this.props.value[caret] === shiftCodesDic[shiftCodes.indexOf(keyCode)]) {
         e.preventDefault();
-        return setSelectionRange(this.refs.search, caret + 1, caret + 1);
+        setSelectionRange(this.refs.search, caret + 1, caret + 1);
+        setTimeout(this.forceUpdate.bind(this), 50)
+        return
       }
+      if ('{' === shiftCodesDic[shiftCodes.indexOf(keyCode)]) {
+        e.preventDefault();
+        this.chooseSuggestion('{}', -1)
+        return
+      }
+      if ('(' === shiftCodesDic[shiftCodes.indexOf(keyCode)]) {
+        e.preventDefault();
+        this.chooseSuggestion('()', -1)
+        return
+      }
+      if ('"' === shiftCodesDic[shiftCodes.indexOf(keyCode)]) {
+        e.preventDefault();
+        this.chooseSuggestion('""', -1)
+        return
+      }
+    }
+
+    if (suggestions.length === 0 && keyCode === 9) {
+      e.preventDefault();
+      return this.chooseSuggestion('  ', 1)
+    }
+
+    if (ctrlKey && keyCode === 13) {
+      e.preventDefault();
+      handleSendQuery();
     }
 
     if (suggestions.length > 0 && (keyCode === 40 || keyCode === 38 || keyCode === 13 || keyCode === 9)) {
       e.preventDefault();
-      if (keyCode === 40 && activeSuggestion + 1 < suggestions.length) {
+      if (keyCode === 40 && activeSuggestion + 1 < suggestions.length && showSuggestion !== false) {
         return this.setState({ activeSuggestion: activeSuggestion + 1 });
       }
       if (keyCode === 38 && activeSuggestion > 0) {
         return this.setState({ activeSuggestion: activeSuggestion - 1 });
       }
       if (keyCode === 13 || keyCode === 9) {
-        console.dir(suggestions[activeSuggestion]);
         return this.chooseSuggestion(suggestions[activeSuggestion].next, suggestions[activeSuggestion].caretOffset);
       }
     }
+    this.forceUpdate();
+  }
+  handleSetCaret() {
+    const caret = getCaret(this.refs.search);
+    this.setState({ caret });
+  }
+  handleOnScroll(e) {
+    this.forceUpdate();
   }
   componentWillReceiveProps(props) {
     const { value, currentDb } = props;
@@ -77,9 +135,9 @@ class Search extends Component {
   }
   render() {
     const { handleOnChange, value, handleSendQuery, currentDb } = this.props;
-    const { activeSuggestion, showSuggestion } = this.state;
+    const { activeSuggestion, showSuggestion, caret } = this.state;
 
-    const { caret } = this;
+    const { handleOnScroll, handleSetCaret } = this;
 
     const collections = currentDb ?
       currentDb.collections.map(v => v.name) : [];
@@ -89,6 +147,7 @@ class Search extends Component {
       setTimeout(() => {
         const position = caret + caretOffset + suggestion.length;
         setSelectionRange(this.refs.search, position, position);
+        this.suggestions = [];
         this.setState({ showSuggestion: false });
       })
     }
@@ -102,30 +161,107 @@ class Search extends Component {
 
     const { offsetHeight, scrollTop } = this.refs.search ? this.refs.search : { offsetHeight: 0, scrollTop: 0 };
 
-    const handleOnFocus = () => this.setState({ showSuggestion: true });
+    const handleOnFocus = () => {
+      this.setState({ showSuggestion: true });
+      handleSetCaret();
+    }
     const handleOnBlur = () => this.setState({ showSuggestion: false });
 
     return (
       <div style={{ position: 'relative' }}>
-        <div className="input-group">
+        <div className="input-group" style={{ overflow: 'hidden' }}>
           <textarea
             onFocus={handleOnFocus}
             onBlur={handleOnBlur}
+            onScroll={handleSetCaret}
+            onClick={handleSetCaret}
             ref="search"
             className="form-control"
             type="text"
             onChange={handleSearchChange}
             value={value}
             aria-describedby="qyinput"
-            style={{ fontFamily: "monospace" }}
+            style={{
+              fontFamily: "monospace",
+              color: 'transparent',
+            }}
           >
           </textarea>
           <span className="input-group-addon" id="qyinput" onClick={handleSendQuery}>Send</span>
+            <TextOverlay value={value} scrollTop={scrollTop} caret={caret} />
         </div>
-        {suggestions.length > 0 && showSuggestion && getSuggestionHtml(suggestions, value, activeSuggestion, caret, chooseSuggestion, scrollTop)}
+        {suggestions.length > 0 /*&& showSuggestion*/ && getSuggestionHtml(suggestions, value, activeSuggestion, caret, chooseSuggestion, scrollTop)}
       </div>
     )
   }
+}
+
+const TextOverlay = ({ value, scrollTop, caret }) => {
+  const style = {
+    position: 'absolute',
+    top: `${8 - scrollTop}px`,
+    zIndex: '2',
+    left: '13px',
+    fontFamily: 'monospace',
+    fontSize: '14px',
+    lineHeight: '1.42857143',
+    pointerEvents: 'none',
+  }
+  return (
+    <div style={style}>
+      {test(value, caret)}
+    </div>
+  )
+}
+
+const colors = {
+  blue: { color: 'blue' },
+  cursor: { letterSpacing: '-4px', marginLeft: '-4px', textDecoration: 'blink' },
+};
+
+const test = (value, caret, idx = 0, res = []) => {
+  console.log(value, caret, idx);
+  if (!value) return [<span style={colors.cursor} className="blink">|</span>];
+  if (caret === idx) {
+    res.push(<span>{value[idx]}<span style={colors.cursor} className="blink">|</span></span>);
+    return test(value, caret, idx + 1, res);
+  }
+  // if (caret )
+  // if (caret === idx + 1) {
+  //   res.push(<span><span style={colors.cursor} className="blink">|</span>{value[idx]}</span>);
+  //   return test(value, caret, idx + 1, res);
+  // }
+  if (!value[idx]) return res;
+
+  res.push(<span>{value[idx]}</span>)
+  return test(value, caret, idx + 1, res);
+  // return <div></div>
+  // if (!value) return [<span style={colors.cursor} className="blink">|</span>];
+  // // if (idx + 1 === value.length) {
+  // //   if (caret === idx + 1) res.push(<span style={colors.cursor} className="blink">|</span>);
+  // //   res.push(<span style={colors.cursor} className="blink">|</span>)
+  // //   return res;
+  // // }
+  // if (caret === idx) {
+  //   res.push(<span style={colors.cursor} className="blink">|</span>);
+  //   return test(value, caret, idx + 1, res);
+  // }
+  //
+  // res.push(<span>{value[idx]}</span>)
+  // return test(value, caret, idx + 1, res);
+}
+
+const formatOverlay = (value, caret) => {
+  if (!value) return <span className="blink">|</span>
+  return value.split('').map((v, idx) => {
+    const hasCaret = caret === idx ? <span style={colors.cursor} className="blink">|</span> : '';
+    const lastCaret = caret === idx + 1 ? <span style={colors.cursor} className="blink">|</span> : '';
+    if (v === '\n') return <span key={idx}>{hasCaret}<br/>{lastCaret}</span>;
+    if (v === ' ') return <span key={idx}>{hasCaret}&nbsp;{lastCaret}</span>;
+    if (v === '{' || v === '}') return <span>{hasCaret}<span key={idx} style={colors.blue}>{v}</span>{lastCaret}</span>;
+
+    return <span key={idx}>{hasCaret}{v}{lastCaret}</span>;
+  })
 }
 
 const formatValue = (value) => value.split('\n').map(v => <span>{v}<br/></span>)
