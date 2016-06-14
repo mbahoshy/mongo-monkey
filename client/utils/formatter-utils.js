13f89cbdfb4bdd184238f9ee567e1da430f486d2
Matt Ross <matt@smartsrc.com>
2016-06-13 21:03:03 -0700

    refactor

--- client/utils/formatter-utils.js
+++ client/utils/formatter-utils.js
@@ -9,56 +9,64 @@ const colors = {
   cursor: { letterSpacing: '-4px', marginLeft: '-4px', textDecoration: 'blink', color: 'black' },
 };
 
-const getHtml = (value, idx, caret, hasCaret, endCaret) => {
+const Caret = () => (
+  <span style={colors.cursor} className="blink">|</span>
+)
+
+const showCaret = (caret, idx) => {
+  return caret === idx ? <Caret /> : false;
+}
+
+const getHtml = (value, idx, caret) => {
+
+  const hasCaret = showCaret(caret, idx);
+
   const char = value[idx];
 
   // map caret to string position
   const accepted = ['}', ' ', ',', '\n'];
   const another = [' ', ':'];
   if ((!value[idx - 1] || another.indexOf(value[idx - 1]) !== -1) && (!value[idx + 4] || accepted.indexOf(value[idx + 4]) !== -1) && value.slice(idx, idx + 4) === 'true') {
-    console.dir(value[idx]);
-    console.dir(value[idx + 4]);
     return {
       diff: 4,
-      html: <span key={idx} style={colors.orange}>{hasCaret}{`true`}{endCaret}</span>
+      html: <span key={idx} style={colors.orange}>{hasCaret}{`true`}</span>
     };
   }
 
   if ((!value[idx - 1] || value[idx - 1] === ' ') && value.slice(idx, idx + 5) === 'false') {
     return {
       diff: 5,
-      html: <span key={idx} style={colors.orange}>{hasCaret}{`false`}{endCaret}</span>
+      html: <span key={idx} style={colors.orange}>{hasCaret}{`false`}</span>
     };
   }
 
   if (char === ':' || char === ',') {
     return {
       diff: 1,
-      html: <span key={idx} style={colors.black}>{hasCaret}{char}{endCaret}</span>
+      html: <span key={idx} style={colors.black}>{hasCaret}{char}</span>
     };
   }
 
   if (char === '\n') {
     return {
       diff: 1,
-      html: <span key={idx}>{hasCaret}<br/>{endCaret}</span>
+      html: <span key={idx}>{hasCaret}<br/></span>
     };
   }
   if (char === ' ') {
     return {
       diff: 1,
-      html: <span key={idx}>{hasCaret}&nbsp;{endCaret}</span>
+      html: <span key={idx}>{hasCaret}&nbsp;</span>
     };
   }
   if (char === '{') {
-    const chars = [<span key={idx}>{hasCaret}<span style={colors.blue}>{`{`}</span>{endCaret}</span>];
+    const chars = [<span key={idx}>{hasCaret}<span style={colors.blue}>{`{`}</span></span>];
     let newIdx = idx + 1;
     while (newIdx < value.length) {
-      const newHasCaret = caret === newIdx ? <span style={colors.cursor} className="blink">|</span> : '';
-      const newEndCaret = caret === newIdx + 1 ? <span style={colors.cursor} className="blink">|</span> : '';
+      const newHasCaret = showCaret(caret, newIdx);
       if (value[newIdx] === '}') break;
-      const { html, diff } = getHtml(value, newIdx, caret, newHasCaret, newEndCaret);
-      chars.push(<span key={newIdx}>{newHasCaret}<span style={colors.red}>{html}</span>{newEndCaret}</span>);
+      const { html, diff } = getHtml(value, newIdx, caret);
+      chars.push(<span key={newIdx}>{newHasCaret}<span style={colors.red}>{html}</span></span>);
       newIdx = newIdx + diff;
     }
     return {
@@ -66,20 +74,19 @@ const getHtml = (value, idx, caret, hasCaret, endCaret) => {
       html: <span key={idx}>{[...chars]}</span>
     }
   }
-  if (char === '{' || char === '}') {
+  if (char === '}') {
     return {
       diff: 1,
-      html: <span>{hasCaret}<span key={idx} style={colors.blue}>{char}</span>{endCaret}</span>
+      html: <span>{hasCaret}<span key={idx} style={colors.blue}>{char}</span></span>
     };
   }
 
   if (char === '\'') {
-    const chars = [<span key={idx}>{hasCaret}<span style={colors.green}>{`'`}</span>{endCaret}</span>];
+    const chars = [<span key={idx}>{hasCaret}<span style={colors.green}>{`'`}</span></span>];
     let newIdx = idx + 1;
     while (newIdx < value.length) {
-      const newHasCaret = caret === newIdx ? <span style={colors.cursor} className="blink">|</span> : '';
-      const newEndCaret = caret === newIdx + 1 ? <span style={colors.cursor} className="blink">|</span> : '';
-      chars.push(<span key={newIdx}>{newHasCaret}<span style={colors.green}>{value[newIdx]}</span>{newEndCaret}</span>);
+      const newHasCaret = showCaret(caret, newIdx);
+      chars.push(<span key={newIdx}>{newHasCaret}<span style={colors.green}>{value[newIdx]}</span></span>);
       if (value[newIdx] === '\'') {
         newIdx++;
         break;
@@ -93,12 +100,11 @@ const getHtml = (value, idx, caret, hasCaret, endCaret) => {
   }
 
   if (char === '"') {
-    const chars = [<span key={idx}>{hasCaret}<span style={colors.green}>{`"`}</span>{endCaret}</span>];
+    const chars = [<span key={idx}>{hasCaret}<span style={colors.green}>{`"`}</span></span>];
     let newIdx = idx + 1;
     while (newIdx < value.length) {
-      const newHasCaret = caret === newIdx ? <span style={colors.cursor} className="blink">|</span> : '';
-      const newEndCaret = caret === newIdx + 1 ? <span style={colors.cursor} className="blink">|</span> : '';
-      chars.push(<span key={newIdx}>{newHasCaret}<span style={colors.green}>{value[newIdx]}</span>{newEndCaret}</span>);
+      const newHasCaret = showCaret(caret, newIdx);
+      chars.push(<span key={newIdx}>{newHasCaret}<span style={colors.green}>{value[newIdx]}</span></span>);
       if (value[newIdx] === '"') {
         newIdx++;
         break;
@@ -113,21 +119,20 @@ const getHtml = (value, idx, caret, hasCaret, endCaret) => {
 
   return {
     diff: 1,
-    html: <span key={idx}>{hasCaret}{char}{endCaret}</span>
+    html: <span key={idx}>{hasCaret}{char}</span>
   };
 }
 
 const formatter = (value, caret) => {
-  if (!value) return <span style={colors.cursor} className="blink">|</span>;
+  if (!value) return <Caret />;
   let idx = 0;
   const res = [];
   while (idx < value.length) {
-    const hasCaret = caret === idx ? <span style={colors.cursor} className="blink">|</span> : '';
-    const endCaret = caret === idx + 1 ? <span style={colors.cursor} className="blink">|</span> : '';
-    const { html, diff } = getHtml(value, idx, caret, hasCaret, endCaret);
+    const { html, diff } = getHtml(value, idx, caret);
     res.push(html)
     idx = idx + diff
   }
+  if (caret === value.length) res.push(<Caret />)
 
   return res;
 }
